a714fdd12f83be1ea8ba830ee47dd36504efbbf7
Eugene Bilyy <bilyy@mlsdev.com>
2020-02-13 02:10:14 +0200

    Auto-generated commit

--- src/app/interfaces/comment.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IComment {
-  _id: string;
-  author: string;
-  time: Date;
-  text: string;
-  sub_comments: any;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: `https://eliftech-268023.ey.r.appspot.com/api`
-};
